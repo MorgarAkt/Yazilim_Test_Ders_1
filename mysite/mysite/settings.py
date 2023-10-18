e6c205fbf8eda986eb94f4e3a8010c8c222970e0
Uygar Akat <94040456+MorgarAkt@users.noreply.github.com>
2023-10-19 02:06:42 +0300

    debug true

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a@d=mgcur-#gvt2e&^d5hh(kr@y6^bu#haxt$!^@&y+3cmf_3n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*", "yazilim-test-blog.onrender.com"]
 
